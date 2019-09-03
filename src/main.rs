d1e6674f27793ffeaaa5d225fd65792ab702057a
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2019-09-03 23:41:22 +0300

    Add simple qrcode convertor

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+extern crate clap;
+extern crate image;
+extern crate qrcode;
+
+use clap::{App, Arg};
+use image::Luma;
+use qrcode::QrCode;
+
+fn main() {
+    let args = App::new("grcode")
+        .version("0.1")
+        .about("generates qrcode")
+        .arg(
+            Arg::with_name("url")
+                .short("u")
+                .long("url")
+                .help("Url for qrcode")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("location")
+                .short("l")
+                .long("location")
+                .help("Place to save image")
+                .takes_value(true)
+                .required(true),
+        )
+        .get_matches();
+
+    let url = args.value_of("url").unwrap();
+    let location = args.value_of("location").unwrap();
+
+    let code = QrCode::new(url).unwrap();
+    let image = code.render::<Luma<u8>>().build();
+
+    image.save(location).unwrap();
+}
